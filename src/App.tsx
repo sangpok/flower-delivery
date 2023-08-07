e40996e3760faa3d4324741ac892b08142c73593
sangpok <y_y5760y_y@naver.com>
2023-08-07 13:39:43 +0900

    tailwind configure

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,21 @@
 function App() {
-  return <h1 className="heading1">hihi</h1>;
+  return (
+    <>
+      <h1 className="heading1">Heading1</h1>
+      <h2 className="heading2">Heading2</h2>
+      <h3 className="heading3">Heading3</h3>
+      <h4 className="heading4">Heading4</h4>
+      <h5 className="heading5">Heading5</h5>
+      <h6 className="heading6">Heading6</h6>
+      <p className="subtitle">Subtitle</p>
+      <p className="body">Body</p>
+      <button className="button block">button</button>
+      <a className="links">links</a>
+      <p className="overline">Overline</p>
+      <p className="caption">caption</p>
+      <p className="caption-small">caption-small</p>
+    </>
+  );
 }
 
 export default App;
